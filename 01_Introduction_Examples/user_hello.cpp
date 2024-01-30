09ca835c7df8e3f660282e224689460cbc61452f
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-01-30 15:25:51 +0000

    updated

---
+++ 01_Introduction_Examples/user_hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout <<endl;
+
+    string name;                     //Create and store string variables
+
+    cout <<"What is your name? ";
+    cin >> name;                    //console input >>
+    cout << "Hello " << name <<endl;
+
+    cout <<endl;
+    return 0;
+}
---
+++ demo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() //main function
+{
+    cout<<"\n" <<"Hello"<<endl;
+    cout<<"Hi. \nHow are you?"<<endl;
+
+    return 0;
+}
