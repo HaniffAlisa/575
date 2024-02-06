6b3d9fbe7b2096cb1f6664e7e4de57b58f3746c6
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-02-06 15:05:15 +0000

    swap

--- demo.cpp
+++ demo.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 int main() //main function
 {
-    cout<<"\n" <<"Hello"<<endl;
-    cout<<"Hi. \nHow are you?"<<endl;
+
+    string a = "A";
+    string b = "B";
+
+    //swap the variable of both variables
+    string temp = a;   //backup first varibles
+    a = b;             //replace first with second
+    b = temp;           //replace second with backup(original first)
+
+    cout << a << " " << b << endl;
+   
+    cout << endl;
 
     return 0;
 }
