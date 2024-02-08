15d92537adf69d5eabc8b66fe2225774166b5714
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-02-08 14:57:32 +0000

    lecture 3 pp

---
+++ 03_Variables_Examples/pp3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    cout << endl;
+
+    double d = 5.3;
+    bool b = 0;
+    int x = b = 11.3;
+    cout << x << endl;
+
+
+
+
+    return 0;
+}
