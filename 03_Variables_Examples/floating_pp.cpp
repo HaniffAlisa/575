fa27b2c602f273f2048f8175d282b738c324ccf8
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-02-08 15:55:15 +0000

    update casting

---
+++ 03_Variables_Examples/floating_pp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout <<endl;
+
+    float f;
+
+    cout << "Enter a floating point value: " << endl;
+    cin >> f;
+
+
+
+    int i;
+    cout << (i=f) << endl;
+    cout << (static_cast<int>(f)) << endl;
+
+
+    
+
+
+
+
+    return 0;
+}
