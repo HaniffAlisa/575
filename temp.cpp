d41ae9e57b4e09fa35215f715acf8a3191753c7c
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-02-15 14:39:24 +0000

    test question

--- temp.cpp
+++ temp.cpp
@@ -4,13 +4,26 @@ using namespace std;
 int main(){
     cout <<endl;
 
-    int x;
-    cout << x%19 <<endl;
-    x%6 -23
+    int x, y ;
+    cout << "Enter an intrger x: ";
+    cin >> x;
 
+    cout << "Enter an integer y: ";
+    cin >> y;
 
+    string s = "Hello"; 
 
+    cout << (x % 20) <<endl;   //inclusive 23 24 25 26 27 28 (max-min +1)
+    
+    cout << (x % 6 + 23) <<endl;
 
+    cout << (static_cast<float>(x)/y) << "\n";  //conversion first then do the division
+
+    // #4 'e' character for string hello
+
+    cout << (x % 5 -3) << endl;
+
+    
 
 
 
