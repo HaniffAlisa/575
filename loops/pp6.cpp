efc93e4d19bd6164234559047f522ef5b5ca52d0
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-03-14 14:04:40 +0000

    updated

---
+++ loops/pp6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << endl;
+
+    int x=10;
+
+    while(x!=0){
+        cout << (x)<<endl;
+        --x;
+    }         
+    cout << "Happy New Year!<<end";
+
+    return 0;
+}
+
+//for loop
+//for (int i=10; i>0; --i)
---
+++ loops/pp8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << endl;
+
+    int i=40;
+
+    for(i=40; i>=20; i-=2){
+        cout << i<<endl;
+    
+
+    }
+    return 0;
+}
