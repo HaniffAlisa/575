617c9df92d485e63d67a13063480460c36036ffc
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-03-26 14:39:39 +0000

    update

---
+++ loops/algoSqrt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << endl;
+
+    int n, count =0;
+    bool isPrime = true;
+
+    cout << " Enter a value for n: " << endl;
+    cin >> n ;
+   
+    for(int i=2 ; i<n; ++i)
+       if(n%i == 0){
+        isPrime = false;
+        break;
+       }
+
+        if(isPrime == true){
+            cout << n << "  prime number." << endl;
+        }
+            else {
+                cout << n << " is not a prime number"<< endl;
+            }
+        
+    
+    return 0;
+}
+
+
