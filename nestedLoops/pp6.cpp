e976b8e571b4222003bb54f6492c6a6c64a2a1f6
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-03-28 14:40:37 +0000

    update

--- nestedLoops/pp6.cpp
+++ nestedLoops/pp6.cpp
@@ -8,10 +8,10 @@ int main() {
 
     for(int i=0; i<DIM; ++i) {          // print rows
         for(int j=0; j<DIM; ++j) {      
-            if(j>2||i>2) && (j==DIM -1-i) {                  // left diagnial
+            if((i>=DIM/2) && (j==i || j==DIM-1-i)){                  // left diagnial
                 cout << "X ";
             } else {
-                cout << "  ";
+                cout << "0 ";
             }
         }
         cout << "\n";
