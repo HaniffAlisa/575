52eab1fcd232fc3f1379060180b5db08e7c751dc
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-02 14:43:30 +0000

    up

--- nestedLoops/algo7.cpp
+++ nestedLoops/algo7.cpp
@@ -4,20 +4,22 @@ using namespace std;
 int main() {
     cout << endl;
 
-    const int n = 4;
+    const int n = 3;
 
     for(int b=0; b<n; ++b) {          
         for(int i=0; i<n; ++i) {   
             for(int j=0; j<n; ++j)   {
-            if(j<=i) {                  
+            if(b%2 ==0 && j<=n-1-i) {                  
                 cout << "X ";
-            } 
-            cout <<"\n ";
-        }  
-        cout << "\n";
+            } else if(b%2 !=0 && j>=i){
+                cout << "X ";
+            }else{
+                cout <<"  ";
+            }
+            
+     cout << "\n";
     }
-
-    cout << endl;
+        }
     }
     return 0;
 }
