dc5c86394e9a5638cbd917d707c665685bf322ca
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-02 13:50:17 +0000

    update

--- nestedLoops/algoTriangle.cpp
+++ nestedLoops/algoTriangle.cpp
@@ -7,11 +7,12 @@ int main() {
 
     for(int i=0; i<n; ++i) {         
         for(int j=0; j<2*n-1; ++j) {     
-            if(j>=i && j<2*n-1-i){  
+            if(j>=n-1-i && j){  
                 cout<< "X ";
         }else {
             cout << "  ";
     }
+      cout << "\n";
         }
     }
     cout << endl;
