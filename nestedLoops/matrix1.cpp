fdbd4eae7bc9b687e0f245daf41d9878d8535aa8
AlisaHaniff <alisahaniff95@gmail.com>
2024-04-03 16:36:29 -0400

    update

---
+++ nestedLoops/matrix1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << endl;
+
+    constant int DIM;
+
+    for(int i=0 ; i<DIM; ++i){
+        for (int j=0; j<DIM; ++j){
+            if(i=DIM/2 && j=DIM/2){
+                cout << "X "<<endl;
+            }
+            else{
+                cout<< "O "<< endl;
+            }
+        }
+    }
+
+    return 0;
+}
