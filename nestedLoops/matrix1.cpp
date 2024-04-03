7b9bf507bbf7512186ed8a5d582c5ffd1bf70535
AlisaHaniff <alisahaniff95@gmail.com>
2024-04-03 17:15:30 -0400

    update

--- nestedLoops/matrix1.cpp
+++ nestedLoops/matrix1.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main() {
     cout << endl;
 
-    constant int DIM;
+    const int DIM = 9;
 
     for(int i=0 ; i<DIM; ++i){
         for (int j=0; j<DIM; ++j){
-            if(i=DIM/2 || j=DIM/2){
+            if(i==DIM/2 || j==DIM/2){
                 cout << "X " << endl;
             }
             else{
                 cout<< "O "<< endl;
             }
-        }
+        } cout << "\n";
     }
 
     return 0;
