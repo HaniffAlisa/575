fb485a30c40694a301e7166e654ce144255c0f93
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-07 22:45:37 +0000

    update

---
+++ nestedLoops/nestingPractice/algo1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout <<endl;
+
+    int n = 0;
+    cout << "Enter a value for n:"<<endl;
+    cin>> n;
+
+    for(int r=1; r<=n; r++){
+        for(int c=1; c<=n; c++){
+            if(c<=n-r+1){
+                cout << "*";
+            }
+        }cout <<"\n";
+    }
+
+    return 0;
+}
