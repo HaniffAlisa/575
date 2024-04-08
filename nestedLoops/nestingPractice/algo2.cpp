ddf619e3d8cadcbb2e053a6b0336192a5a5dfd71
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-08 15:56:05 +0000

    updated

--- nestedLoops/nestingPractice/algo2.cpp
+++ nestedLoops/nestingPractice/algo2.cpp
@@ -10,7 +10,7 @@ int main (){
 
     for(int r=1; r<=n; r++){
         for(int c=1; c<=n; c++){
-            if(r>=c){
+            if(r<=c){
                 cout << "*";
             }
         }cout <<"\n";
