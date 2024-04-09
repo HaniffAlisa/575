f6e3010a3bda3f0b39600e734b019688ca271039
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-09 13:42:08 +0000

    update

--- functions/pp1.cpp
+++ functions/pp1.cpp
@@ -9,6 +9,25 @@ bool isEven(int n);
 int main(){
     cout<< endl;
 
+    cout <<square(5)<<"\n";     //compute 5*5
+    cout << multiply(5,6)<<"\n";    //compute 5*6
+    hello("John");              //print Hello John
+    cout<<isEven(4) << "/n";
+
+    cout<<endl;
+    return 0;
+}
+int square(intn) {
+    return multiply(n.n);
+}
+int multiply(int x, int y){
+    return x*y;
+}
+void hello(string  s) {
+    cout<< "Hello " + s << "\n";
+    
+}
+
 
 return 0;
 }
