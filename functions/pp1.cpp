dfecd9102fa162923dac4d6e4d307a9a70f0f3b7
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-09 13:24:52 +0000

    update

---
+++ functions/pp1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int square (int * int){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    return n;
+}
+
+int main(){
+    cout<< square() << endl;
+}
