c018e3effb6da991a6f4a67e506b9af04a34cedb
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-11 13:57:43 +0000

    re

--- functions/algo5.cpp
+++ functions/algo5.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int c = 0;
-cout << "Enter a value for nth: "<<endl;
-cin >> c ;
+int nth = 0;
+int c=0;
 
-int fib(int n){
-    
+cout << "Enter a value for nth: "<<endl;
+cin >> nth ;
+
+int fib(int nth){
+    int c=nth+1, p=nth-1;
+    for(int i+1; i<=c; ++i){
+
     }
-   return p; 
+    }
+   return c; 
 }
 int main(){
 
