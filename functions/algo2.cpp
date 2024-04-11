6b2f724b5390044b7014ff5ec87c0efd62ce3ae1
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-11 13:36:22 +0000

    up date

--- functions/algo2.cpp
+++ functions/algo2.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 
-string reverse(){
+string reverse(string s){
+    int start=0, end=s.length()-1;
+    while start
 string s;
 cout<< "Enter a string:"<<endl;
 cin>>s;
---
+++ functions/algo4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+/*
+
+5! = 5*4*3*2*1      
+product = procduct = next digit
+
+*/
+int n;
+cout<< "Enter a value for n: "<<endl;
+cin>>n;
+
+int factorial(n){
+    
+}
+
+
+int main(){
+    cout <<endl;
+
+    
+    cout<<endl;
+
+    return 0;
+}
