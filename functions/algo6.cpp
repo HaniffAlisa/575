e555f9e53aee27c0c9893ba2e6591e748d06955b
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-04-11 14:13:33 +0000

    updated

---
+++ functions/algo6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int n=0;
+
+
+cout<<"Enter a value for n:"<<endl;
+cin>>n;
+
+int factors(int n){
+    if(n >0){
+        cout<<" Enter a positive number"<<endl;
+    }if else(x%n==0){
+        cout(n)
+    }
+    }
+
+
+
+
+int main(){
+
+}
