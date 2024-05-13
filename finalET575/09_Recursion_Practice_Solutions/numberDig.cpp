0be92beee2c6ba56ba50d3c0e6a39d505f67ab04
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-05-13 22:15:01 +0000

    update

--- finalET575/09_Recursion_Practice_Solutions/numberDig.cpp
+++ finalET575/09_Recursion_Practice_Solutions/numberDig.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void countDigits(int n){
+int countDigits(int n){
     if(n<10){
         return 1;
     }
     return countDigits(n/10)+1;
 }
+
+
 int main(){
     cout <<endl;
-
+    int n;
     cout<< "Enter a number with multiple digits: "<<endl;
     cin >> n;
 
