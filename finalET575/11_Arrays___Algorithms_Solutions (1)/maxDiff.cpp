d47cbd75dc8afd8f9d5dfbbb6bf59a9e60680449
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-05-14 13:32:47 +0000

    up

---
+++ finalET575/11_Arrays___Algorithms_Solutions (1)/maxDiff.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<time.h>
+using namespace std;
+
+void init(int *a, int size){
+    for(int i=0; i<size; ++i){
+        a[i]=rand()%21;
+    }
+}
+void print(int *a, int size){
+    for(int i=0; i<size; ++i){
+        cout<< a[i] << " ";
+
+
+int maxConsecutiveDiff(int *a, int size) {
+    int maxDiff = 0;
+    for (int i=1; i<size; ++i) {                // iterate from 2nd to last value
+         int diff = a[i] - a[i-1];              // diff == current - previous (look back)
+         if (diff < 0) diff = -diff;            // find abs value of diff
+         if (diff > maxDiff) maxDiff = diff;    // if diff is greater than maxDiff, maxDiff == diff
+     }
+     return maxDiff;
+}
+
+
+int main(){
+    srand(time(NULL))
+
+    const int SIZE =10;
+    int a[SIZE];
+
+    init(a,SIZE);
+    print(a, SIZE);
+
+    int diff = maxConsecutiveDiff(a, SIZE);
+
+    cout << "\nMax difference = " << diff << endl;
+
+    cout << endl;
+    cout<< 
+
+return 0;
+}
